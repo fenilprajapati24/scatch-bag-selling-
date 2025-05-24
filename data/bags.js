2e34c336c0b256141333f82e87e3c9c08277b1d0
fenilprajapati24 <fenilprajapati9334@gmail.com>
2025-05-24 15:13:19 +0530

    data added

---
+++ data/bags.js
@@ -0,0 +1,44 @@
+// data/bags.js
+module.exports= [
+  {
+    name: "Vintage Leather Satchel",
+    image: "/images/1bag.png",
+    price: "$150"
+  },
+  {
+    name: "Classic Canvas Tote",
+    image: "/images/2bag.png",
+    price: "$85"
+  },
+  {
+    name: "Modern Mini Backpack",
+    image: "/images/3bag 1.png",
+    price: "$95"
+  },
+  {
+    name: "Elegant Evening Clutch",
+    image: "/images/4bag.png",
+    price: "$110"
+  },
+  {
+    name: "Eco-Friendly Jute Bag",
+    image: "/images/5bag.png",
+    price: "$45"
+  },
+  {
+    name: "Travel Duffel Bag",
+    image: "/images/6bag.png",
+    price: "$130"
+  },
+  {
+    name: "Luxury Designer Handbag",
+    image: "/images/7bag.png",
+    price: "$250"
+  },
+  {
+    name: "Waterproof Laptop Bag",
+    image:"/images/image 80.png",
+    price: "$90"
+  }
+];
+// module.exports.bags;
